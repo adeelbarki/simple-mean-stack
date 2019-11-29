815b51dc1f19ca5775d6b82622e67084f61851fb
Adeel Ahmed Khan <adeelbarki@gmail.com>
2019-11-29 14:41:01 +0500

    Reactive Form added to input-user-component ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { InputUserDataFormComponent } from './input-user-data-form/input-user-data-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { InputUserDataFormComponent } from './input-user-data-form/input-user-da
     InputUserDataFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
